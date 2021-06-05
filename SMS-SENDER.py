6d18b886d0f47daacaf4b92795d77b66b0f5cd31
Rohan Raj <85404080+Hacker-Rohan-Raj@users.noreply.github.com>
2021-06-05 22:52:22 +0530

    Add files via upload

---
+++ SMS-SENDER.py
@@ -0,0 +1,41 @@
+import requests
+print("Made with Love by ❤️❤️❤️Rohan Raj❤️❤️❤️")
+print("Made with Love by ❤️❤️❤️Rohan Raj❤️❤️❤️")
+print("Version 1.0")
+print("\033[91m Checking dependencies... \033[0m")
+time.sleep(2)
+os.system("bash Requirements.sh")
+def menu() :
+    print(Send a Message to Any Number\")
+    print("Check if the Message is Delivered or not")
+def control() :
+    ctrl = input("What are You Gonna Choose : ")
+    if ctrl == "1" :
+        sms()
+    elif ctrl == "2" :
+        status()
+    else :
+        print("Invalid number")
+def sms() :
+   phone_no = input("Please Enter Your Country Code and Phone Number With a Plus     \n    Example :- +911122334455: ")
+   msg = input("message to send : ")
+
+   resp = requests.post('https://textbelt.com/text',{
+	'phone' : phone_no,
+	'message' : msg ,
+	'key' : 'textbelt'
+   })
+
+   print(resp.text)
+   if '"success" : true' in resp.text :
+       print('Your Message is Delivered! ')
+   if '"success" : false' in resp.text :
+       print("Failed to Send Message!\n Sorry!! Try again!! ")
+def status() :
+  textID = input("Enter textID of sms : ") 
+  os.system(f"curl https://textbelt.com/status/{textID}")
+os.system("clear")
+os.system("toilet -f smblock --filter border:metal 'Free SMS_Sender' ")
+print("\033[96mCoded by - Rohan Raj")
+menu()
+control()
